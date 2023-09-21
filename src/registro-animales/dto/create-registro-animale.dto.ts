730f5fef3203551244534946e39b1d4a5ae432f5
Davila99 <davilaeliseo453@gmail.com>
2023-09-20 23:44:22 -0600

    creando las entitys

--- src/registro-animales/dto/create-registro-animale.dto.ts
+++ src/registro-animales/dto/create-registro-animale.dto.ts
@@ -1,1 +1,52 @@
-export class CreateRegistroAnimaleDto {}
+import {
+  IsNotEmpty,
+  IsString,
+  IsNumberString,
+  IsOptional,
+} from 'class-validator';
+export class CreateRegistroAnimaleDto {
+  @IsOptional()
+  @IsNumberString()
+  id: number;
+
+  @IsNotEmpty()
+  @IsString()
+  foto : string;
+
+  @IsNotEmpty()
+  @IsString()
+  fechaNacimiento : string;
+
+  @IsNotEmpty()
+  @IsString()
+  raza : string;
+
+  @IsNotEmpty()
+  @IsString()
+  sexo : string;
+
+  @IsNotEmpty()
+  @IsString()
+  color : string;
+
+  @IsNotEmpty()
+  @IsString()
+  padres : string;
+
+  @IsNotEmpty()
+  @IsString()
+  registroVacunas : string;
+
+  @IsNotEmpty()
+  @IsString()
+  peso : string;
+
+  @IsNotEmpty()
+  @IsString()
+  historialClinico : string;
+  
+  @IsNotEmpty()
+  @IsString()
+  estadoReproductivo : string;
+
+}
--- src/registro-animales/entities/registro-animale.entity.ts
+++ src/registro-animales/entities/registro-animale.entity.ts
@@ -1,1 +1,37 @@
-export class RegistroAnimale {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+@Entity()
+export class RegistroAnimale {
+    @PrimaryGeneratedColumn()
+    id: number;
+  
+    @Column()
+    foto : string;
+
+    @Column()
+    fechaNacimiento : string;
+
+    @Column()
+    raza : string;
+
+    @Column()
+    sexo : string;
+
+    @Column()
+    color : string;
+
+    @Column()
+    padres : string;
+
+    @Column()
+    registroVacunas : string;
+
+    @Column()
+    peso : string;
+
+    @Column()
+    historialClinico : string;
+    
+    @Column()
+    estadoReproductivo : string;
+    
+}
