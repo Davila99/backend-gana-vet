4422395c348c707c52eb16accd81cff0347c9a3d
Davila99 <davilaeliseo453@gmail.com>
2023-09-23 15:10:53 -0600

    Update create-categoria-animale.dto.ts

--- src/categoria-animales/dto/create-categoria-animale.dto.ts
+++ src/categoria-animales/dto/create-categoria-animale.dto.ts
@@ -1,10 +1,14 @@
-import { IsNotEmpty, IsString,  IsNumberString,
-    IsOptional } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsString,
+  IsNumberString,
+  IsOptional,
+} from 'class-validator';
 
 export class CreateCategoriaAnimaleDto {
-@IsOptional()
- @IsNumberString()
- id: number;
+  @IsOptional()
+  @IsNumberString()
+  id: number;
 
   @IsNotEmpty()
   @IsString()
